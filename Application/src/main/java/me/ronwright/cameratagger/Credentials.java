052f3e789044e3128db13986f29a35207c4f57d6
Andrew Xu <andrewxu_2016@depauw.edu>
2016-02-27 18:31:04 -0500

    Initial commit

---
+++ Application/src/main/java/me/ronwright/cameratagger/Credentials.java
@@ -0,0 +1,13 @@
+package me.ronwright.cameratagger;
+
+class Credentials {
+  // IMPORTANT NOTE: you should replace the CLIENT_ID and CLIENT_SECRET values below
+  // with your own. You can obtain these at https://developer.clarifai.com/applications -- the
+  // defaults are shared credentials that will stop working once their quota is reached.
+  //
+  // Also, if you're committing your code to a public repo, be careful not to check in your
+  // credentials -- we're including these here so you can get started out of the box, but this is
+  // generally a bad practice.
+  static final String CLIENT_ID = "6J3en3fBc6nDWHx1M0skJ4bVG-Z-itHjzA4F08Jl";
+  static final String CLIENT_SECRET = "OF5c4cLlxNWi2aJDbnZZlLvaU8Aes1VMm63afJSI";;
+}
